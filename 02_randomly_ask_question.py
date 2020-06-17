8bf205f297309471b9a2062b8f74f6b1ad0727fa
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2020-06-17 12:44:06 +1200

    generating question

---
+++ 02_randomly_ask_question.py
@@ -0,0 +1,14 @@
+# generate two random number
+# put them together in an equation (n + n = )
+# get users input
+# check if it's right or wrong
+
+import random
+
+# generating two random #
+num_one = random.randint(1,10)
+num_two = random.randint(1,10)
+# putting it in an equation
+equation = int(input("Please answer the question {} + {} = ".format(num_one,num_two)))
+
+
